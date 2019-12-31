8b1d241adb7e01775b7b6feec88413fdccced498
richardmarquez2001 <richard.marquez@ryerson.ca>
2019-12-31 02:14:00 -0500

    test fix_2

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const Discord = require('discord.js');
 
-//const token = 'NjU2OTkzMjE3NzUzMTIwNzg5.XgrmUg.RHWg06f_ZZJdZopw0nId9Gsk7mU';
+const token = 'NjU2OTkzMjE3NzUzMTIwNzg5.XgrmUg.RHWg06f_ZZJdZopw0nId9Gsk7mU';
 
 const PREFIX = '!';
 
 require('dotenv/config');
 
-const http = ('http');
+// const http = ('http');
 
 const port = process.env.PORT || 3000;
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+//
+//   console.log("I am working");
+// }).listen(port);
 
-  console.log("I am working");
-}).listen(port);
-
-const token = process.env.token;
+//const token = process.env.token;
 
 const bot = new Discord.Client();
 
